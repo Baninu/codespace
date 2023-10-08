f5bdbd2352944d10d0168822cada7a268f25a8e4
Baninu <87016293+Baninu@users.noreply.github.com>
2023-10-08 12:58:37 +0000

    Sun, Oct 8, 2023, 12:58 PM +00:00

--- test.c
+++ test.c
@@ -6,7 +6,7 @@ int main (void)
     int num = get_int("Numbers of animals:");
     int birth = num/3;
     int death = num/4;
-    printf("Original number:%d ",num,",Birth:%d ",birth,",Death: %d",death);
+    printf("Original number:%d ",num,",Birth: ",birth,",Death: ",death);
 
 
 }
